a918bf50a397187269647c861d6fab870e15b26a
abhisheksharma <abhisheksharma131098@gmail.com>
2023-07-14 12:06:26 +0530

    create navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <h1>Nav</h1>
+        <Navbar/>
         {children}
       </body>
     </html>
